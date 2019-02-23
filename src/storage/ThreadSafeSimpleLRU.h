19276bd26c31cd1aabf0dc9e279502c64f356771
Sergey Protserov <sergey.protserov@gmail.com>
2019-02-23 15:52:19 +0300

    ThreadSafeSimpleLRU written for HW2. Global mutex used, since there are no thread-safe operations.

--- src/storage/ThreadSafeSimpleLRU.h
+++ src/storage/ThreadSafeSimpleLRU.h
@@ -15,6 +15,11 @@ namespace Backend {
  *
  *
  */
+// SINCE ACCORDING TO LRU LOGIC, ELEMENT'S POSITION IN CACHE
+// MUST BE UPDATED ON EACH READ, THERE ARE NO THREAD-SAFE
+// OPERATIONS AT ALL.
+// AS SUCH, I WILL USE SINGLE GLOBAL MUTEX TO PROTECT
+// EACH AND EVERY OPERATION.
 class ThreadSafeSimplLRU : public SimpleLRU {
 public:
     ThreadSafeSimplLRU(size_t max_size = 1024) : SimpleLRU(max_size) {}
@@ -22,37 +27,37 @@ public:
 
     // see SimpleLRU.h
     bool Put(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
+        std::lock_guard<std::mutex> lg(_m);
         return SimpleLRU::Put(key, value);
     }
 
     // see SimpleLRU.h
     bool PutIfAbsent(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
+        std::lock_guard<std::mutex> lg(_m);
         return SimpleLRU::PutIfAbsent(key, value);
     }
 
     // see SimpleLRU.h
     bool Set(const std::string &key, const std::string &value) override {
-        // TODO: sinchronization
+        std::lock_guard<std::mutex> lg(_m);
         return SimpleLRU::Set(key, value);
     }
 
     // see SimpleLRU.h
     bool Delete(const std::string &key) override {
-        // TODO: sinchronization
+        std::lock_guard<std::mutex> lg(_m);
         return SimpleLRU::Delete(key);
     }
 
     // see SimpleLRU.h
     // Get is no longer const, since according to LRU logic, it should update element's position
     bool Get(const std::string &key, std::string &value) override {
-        // TODO: sinchronization
+        std::lock_guard<std::mutex> lg(_m);
         return SimpleLRU::Get(key, value);
     }
 
 private:
-    // TODO: sinchronization primitives
+    std::mutex _m;
 };
 
 } // namespace Backend

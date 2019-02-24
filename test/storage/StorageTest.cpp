ac236c78c9de259eeafe26041356aa8b83696cf3
Sergey Protserov <sergey.protserov@gmail.com>
2019-02-24 18:21:46 +0300

    Add one more storage test for multiple overwrites

--- test/storage/StorageTest.cpp
+++ test/storage/StorageTest.cpp
@@ -79,6 +79,29 @@ TEST(StorageTest, BigTest) {
     }
 }
 
+TEST(StorageTest, BigOverwrite) {
+    const size_t length = 20;
+    SimpleLRU storage(2 * 1000 * length);
+
+    for (long i = 0; i < 1000; ++i) {
+        for (long j = 0; j < 11; ++j) {
+            auto key = pad_space("Key " + std::to_string(i), length);
+            auto val = pad_space("Val " + std::to_string(i * 70 * j), length);
+            storage.Put(key, val);
+        }
+    }
+
+    for (long i = 999; i >= 0; --i) {
+        auto key = pad_space("Key " + std::to_string(i), length);
+        auto val = pad_space("Val " + std::to_string(i * 70 * 10), length);
+
+        std::string res;
+        EXPECT_TRUE(storage.Get(key, res));
+
+        EXPECT_TRUE(val == res);
+    }
+}
+
 TEST(StorageTest, MaxTest) {
     const size_t length = 20;
     SimpleLRU storage(2 * 1000 * length);
